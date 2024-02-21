ae9cae37500872640ae216c697720ccc47e454f4
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-02-21 17:43:24 +0700

    feat: create constant navigation

---
+++ src/constant/navigationMenus.js
@@ -0,0 +1,71 @@
+import { siteConfig } from "./siteConfig"; 
+import {
+  Activity,
+  CodeIcon,
+  CoffeeIcon,
+  HomeIcon,
+  PhoneIcon,
+  Settings,
+  UserIcon,
+} from "lucide-react";
+
+export const NAVIGATION = [
+  {
+    title: "Home",
+    path: "/",
+    content: "_Home",
+    icon: HomeIcon,
+  },
+  {
+    title: "About",
+    path: "/about",
+    content: "_About",
+    icon: UserIcon,
+    children: [
+      {
+        name: "_Personal",
+        path: "/about/personal.js",
+      },
+      {
+        name: "_Tech-stack",
+        path: "/about/tech-stack.js",
+      },
+    ]
+  },
+  {
+    title: "Projects",
+    path: "/projects",
+    content: "_Projects",
+    icon: CodeIcon,
+  },
+  {
+    title: "Activity",
+    path: "/activity",
+    content: "_Activity",
+    icon: Activity,
+  },
+  {
+    title: "Contact",
+    path: "/",
+    content: "_Contact",
+    icon: PhoneIcon,
+    children: [
+      {
+        name: "Email",
+        path: siteConfig.links.email,
+      },
+      {
+        name: "Github",
+        path: siteConfig.links.github,
+      },
+      {
+        name: "Linkedin",
+        path: siteConfig.links.linkedin,
+      },
+      {
+        name: "Instagram",
+        path: siteConfig.links.instagram,
+      },
+    ]
+  },
+]
---
+++ src/constant/siteConfig.js
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "Haikal Ramadhan",
+  description: "Haikal Ramadhan personal website",
+  username: "haikalramadhan",
+  url: "http://localhost:3000",
+  repository: "https://",
+  links: {
+    github: "https://github.com/",
+    linkedin: "https://www.linkedin.com/in/haikalramadhan",
+    instagram: "https://www.instagram.com/soon.haikal",
+    email: "mailto:haikalramadhan1015@gmail.com",
+  },
+};
