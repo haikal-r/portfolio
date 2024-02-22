2855c19a615dc2a9695e270df5d6c991c751cdcc
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-02-22 22:36:28 +0700

    feat: slicing about page layout

---
+++ src/app/about/layout.jsx
@@ -0,0 +1,32 @@
+import { Accordion, AccordionContent, AccordionTrigger, AccordionItem } from "@/components/atoms/accordion";
+
+
+export default function AboutLayout() {
+  return (
+    <section className="h-full overflow-hidden grid grid-cols-12">
+      <aside className="hidden overflow-y-auto border-r md:col-span-3 md:block lg:col-span-2">
+        <Accordion type="single" collapsible defaultValue="about">
+          <AccordionItem value={"about"} defaultChecked>
+            <AccordionTrigger className="border-b border-lines px-5 py-2.5 text-left">
+              About Me
+            </AccordionTrigger>
+            <AccordionContent className='mt-5 space-y-1'>
+
+            {/* {allAbouts.map(({ title }) => (
+                  <FadeIn key={title}>
+                    <Suspense fallback={<>Loading...</>}>
+                      <AsideLink href={title} key={title} title={title}>
+                        <IoLogoJavascript className="w-4 h-4 shrink-0" />
+                        {title}
+                      </AsideLink>
+                    </Suspense>
+                  </FadeIn>
+                ))} */}
+
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </aside>
+    </section>
+  );
+}
