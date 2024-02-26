3f8c957498d22cfe9011b5699148532bd9f3cc20
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-02-26 18:07:08 +0700

    feat: slicing project page

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -7,21 +7,27 @@ import ProjectCard from "@/components/molecules/project-card";
 
 export default function ProjectsPage() {
   return (
-    <section className="overflow-y-auto relative h-full p-5">
-    {/* <h3 className="text-lg mb-3">PROJECTS</h3> */}
+    <section className="relative h-full overflow-y-auto p-5">
+      {/* <h3 className="text-lg mb-3">PROJECTS</h3> */}
 
-    <FadeInStagger
-      className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5"
-      faster
-    >
-      <AnimatePresence mode="wait">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-      </AnimatePresence>
-    </FadeInStagger>
+      <FadeInStagger
+        className="grid gap-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+        faster
+      >
+        <AnimatePresence mode="wait">
+          <FadeIn
+            layout
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+          >
+            <ProjectCard />
+          </FadeIn>
+        </AnimatePresence>
+      </FadeInStagger>
 
-    <br />
-  </section>
+      <br />
+    </section>
   );
 }
