0a2c893494a223cb072fc5736775863de881ebd1
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-02-26 18:03:14 +0700

    feat: add layout about page

--- src/app/about/layout.jsx
+++ src/app/about/layout.jsx
@@ -1,31 +1,42 @@
-import { Accordion, AccordionContent, AccordionTrigger, AccordionItem } from "@/components/atoms/accordion";
+import { Suspense } from "react";
+import { allAbouts } from "contentlayer/generated";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/atoms/accordion";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FadeInStagger, FadeIn } from "@/components/atoms/fade-in";
+import { AsideLink } from "@/components/atoms/aside-link";
 
-export default function AboutLayout() {
+export default function AboutLayout({ children }) {
   return (
-    <section className="h-full overflow-hidden grid grid-cols-12">
+    <section className="grid h-full grid-cols-12 overflow-hidden">
       <aside className="hidden overflow-y-auto border-r md:col-span-3 md:block lg:col-span-2">
         <Accordion type="single" collapsible defaultValue="about">
           <AccordionItem value={"about"} defaultChecked>
-            <AccordionTrigger className="border-b border-lines px-5 py-2.5 text-left">
+            <AccordionTrigger className="border-lines border-b px-5 py-2.5 text-left">
               About Me
             </AccordionTrigger>
-            <AccordionContent className='mt-5 space-y-1'>
-
-            {/* {allAbouts.map(({ title }) => (
-                  <FadeIn key={title}>
-                    <Suspense fallback={<>Loading...</>}>
-                      <AsideLink href={title} key={title} title={title}>
-                        <IoLogoJavascript className="w-4 h-4 shrink-0" />
-                        {title}
-                      </AsideLink>
-                    </Suspense>
-                  </FadeIn>
-                ))} */}
-
+            <AccordionContent className="mt-5 space-y-1">
+              {allAbouts.map(({ title }) => (
+                <FadeIn key={title} >
+                  <Suspense fallback={<>Loading...</>}>
+                    <AsideLink href={title} key={title} title={title}>
+                    <IoLogoJavascript className="h-[17px] w-[17px] shrink-0" />
+                    {title}
+                    </AsideLink>
+                  </Suspense>
+                </FadeIn>
+              ))}
             </AccordionContent>
           </AccordionItem>
         </Accordion>
       </aside>
+      <section className="md:col-span-9 lg:col-span-10 col-span-12 overflow-y-auto relative h-[84dvh] md:h-auto">
+        {children}
+      </section>
     </section>
   );
 }
